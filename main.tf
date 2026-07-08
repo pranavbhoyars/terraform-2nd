fb7021295b82fdd3c424ba314ac3088b50559cc7
root <root@ip-172-31-24-186.ap-southeast-2.compute.internal>
2026-07-08 08:38:00 +0000

    push

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  # It is highly recommended to configure a remote S3 backend here for shared state
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "infrastructure" {
+  source   = "./modules/vpc_ec2"
+  region   = var.aws_region
+}
