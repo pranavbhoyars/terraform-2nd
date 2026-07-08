8f0baf93eb6f4b2a85fc5fd362475e4f5fc812cf
root <root@ip-172-31-24-186.ap-southeast-2.compute.internal>
2026-07-08 08:44:55 +0000

    pushmy data

---
+++ modules/vpc_ec2/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  type    = string
+  default = "ap-southeast-2"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  type    = string
+  default = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
