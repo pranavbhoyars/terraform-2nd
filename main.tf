44d5ad6b5b1a110dcbc67b730a6d5e077b78a94d
pranavbhoyars <121799012+pranavbhoyars@users.noreply.github.com>
2026-07-11 18:05:10 +0530

    Remove version constraint for AWS provider
    Removed version constraint for AWS provider.

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      
     }
   }
   # It is highly recommended to configure a remote S3 backend here for shared state
