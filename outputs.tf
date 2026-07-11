2f1774454318a8c43aabe5c87b90d37ae341ef19
pranavbhoyars <121799012+pranavbhoyars@users.noreply.github.com>
2026-07-11 19:29:49 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,6 @@ output "ec2_global_public_ip" {
   value       = module.infrastructure.instance_public_ip
   description = "The public IP of the created EC2 instance"
 }
+output "ecr_repository_url" {
+  value = aws_ecr_repository.nginx_repo.repository_url
+}
